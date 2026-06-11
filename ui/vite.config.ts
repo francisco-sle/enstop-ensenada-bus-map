2142e24c0974a2b7fa23f58e6faebf296874dd6b
Francisco Lopez <core_andrum769@outlook.com>
2026-06-10 22:48:02 -0700

    fixes vercel deployment

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,12 +1,18 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   define: {
     'import.meta.env.VITE_USE_MOCKS': JSON.stringify(process.env.VITE_USE_MOCKS ?? 'true')
+  },
+  resolve: {
+    alias: {
+      leaflet: path.resolve(__dirname, 'node_modules/leaflet')
+    }
   },
   optimizeDeps: {
     include: ['leaflet', 'leaflet-rotatedmarker', 'leaflet-polylinedecorator']
