f40abfe5b21381c3c1adb8f321819024f82d70c7
Francisco Lopez <core_andrum769@outlook.com>
2026-06-20 18:42:11 -0700

    fix(api): force mock mode to ignore prod db url

--- ui/src/api/supabase.ts
+++ ui/src/api/supabase.ts
@@ -2,11 +2,15 @@ import { createClient } from '@supabase/supabase-js'
 import type { Database } from '../types/supabase-generated'
 
 const isMock = import.meta.env.VITE_USE_MOCKS === 'true'
-const defaultUrl =
-  isMock && typeof window !== 'undefined' ? window.location.origin : 'http://127.0.0.1:54321'
+const supabaseUrl = isMock
+  ? typeof window !== 'undefined'
+    ? window.location.origin
+    : 'http://127.0.0.1:54321'
+  : import.meta.env.VITE_SUPABASE_URL || 'http://127.0.0.1:54321'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || defaultUrl
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'dummy_anon_key'
+const supabaseAnonKey = isMock
+  ? 'mock_anon_key'
+  : import.meta.env.VITE_SUPABASE_ANON_KEY || 'dummy_anon_key'
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey)
 export type { Database }
